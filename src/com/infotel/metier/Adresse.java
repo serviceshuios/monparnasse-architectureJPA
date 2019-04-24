74cc6cb73affddb273a6a2ddc0362c072f213921
Eddy CHRISTIAN <services.huios@gmail.com>
2019-04-24 17:02:30 +0200

    modification de la classe Adresse

--- src/com/infotel/metier/Adresse.java
+++ src/com/infotel/metier/Adresse.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.PreRemove;
+// ma classe Adresse
 @Entity
 public class Adresse {
 	@Id
